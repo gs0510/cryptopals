82df01a76995715870ab2e95783f7fc34bb69c8a
Gargi Sharma <gs051095@gmail.com>
2019-04-25 22:35:05 +0100

    smol portion of S1-R3

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 extern crate base64;
 extern crate hex;
+use std::str;
 
 pub fn hex_to_base64(hex: String) -> Result<String, hex::FromHexError> {
     // convert hex string to bytes
@@ -12,13 +13,34 @@ pub fn hex_xor(hex1: String, hex2: String) -> Result<String, hex::FromHexError> 
     let byte_array1 = hex::decode(hex1)?;
     let byte_array2 = hex::decode(hex2)?;
 
-    let mut byte_array = byte_array1
+    let byte_array = byte_array1
         .iter()
         .zip(byte_array2.iter())
         .map(|(&a, &b)| a ^ b)
         .collect::<Vec<_>>();
 
     Ok(hex::encode(byte_array))
+}
+
+pub fn xor_repeats(byte_array1: &Vec<u8>, byte2: &u8) -> Vec<u8> {
+    byte_array1
+        .iter()
+        .zip(vec![byte2].iter().cycle())
+        .map(|(&a, &b)| a ^ b)
+        .collect::<Vec<_>>()
+}
+
+pub fn xor_cipher(hex1: String, byte2: &u8) -> Result<String, hex::FromHexError> {
+    let byte_array1 = hex::decode(hex1)?;
+
+    let result = xor_repeats(&byte_array1, byte2);
+
+    let s = match str::from_utf8(&result) {
+        Ok(v) => v,
+        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+    };
+
+    Ok(s.to_string())
 }
 
 #[cfg(test)]
@@ -33,7 +55,7 @@ mod tests {
         );
     }
 
-     #[test]
+    #[test]
     fn test_hex_xor() {
         assert_eq!(
             hex_xor(
@@ -42,6 +64,19 @@ mod tests {
             ),
             Ok(String::from("746865206b696420646f6e277420706c6179"))
         );
+    }
+
+    #[test]
+    fn test_xor_cipher() {
+        let s = match xor_cipher(
+            String::from("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"),
+            &0b0000_0101,
+        ) {
+            Ok(v) => v,
+            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+        };
+
+        println!("result: {}", s);
     }
 
 }
