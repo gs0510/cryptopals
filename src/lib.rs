d310ac10497ded126937dd94b9a76b55532be58f
Gargi Sharma <gs051095@gmail.com>
2019-05-16 19:57:21 +0100

    Finally the third onegit add src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,8 @@
 extern crate base64;
 extern crate hex;
-use std::str;
 
-// Source:
-// Lee, E. Stewart. "Essays about Computer Security" (PDF). University of Cambridge Computer Laboratory. p. 181.
-static EXPECTED_FREQUENCIES: [(u8, f32); 28] = [
-    (b' ', 12.17), // Whitespace
-    (b'.', 6.57),  // Others
-    (b'a', 6.09),
-    (b'b', 1.05),
-    (b'c', 2.84),
-    (b'd', 2.92),
-    (b'e', 11.36),
-    (b'f', 1.79),
-    (b'g', 1.38),
-    (b'h', 3.41),
-    (b'i', 5.44),
-    (b'j', 0.24),
-    (b'k', 0.41),
-    (b'l', 2.92),
-    (b'm', 2.76),
-    (b'n', 5.44),
-    (b'o', 6.00),
-    (b'p', 1.95),
-    (b'q', 0.24),
-    (b'r', 4.95),
-    (b's', 5.68),
-    (b't', 8.03),
-    (b'u', 2.43),
-    (b'v', 0.97),
-    (b'w', 1.38),
-    (b'x', 0.24),
-    (b'y', 1.30),
-    (b'z', 0.03),
-];
+use std::collections::HashMap;
+
 
 pub fn hex_to_base64(hex: String) -> Result<String, hex::FromHexError> {
     // convert hex string to bytes
@@ -55,7 +24,7 @@ pub fn hex_xor(hex1: String, hex2: String) -> Result<String, hex::FromHexError> 
     Ok(hex::encode(byte_array))
 }
 
-pub fn xor_repeats(byte_array1: &Vec<u8>, byte2: &u8) -> Vec<u8> {
+pub fn xor_repeats(byte_array1: &[u8], byte2: &u8) -> Vec<u8> {
     byte_array1
         .iter()
         .zip(vec![byte2].iter().cycle())
@@ -63,23 +32,63 @@ pub fn xor_repeats(byte_array1: &Vec<u8>, byte2: &u8) -> Vec<u8> {
         .collect::<Vec<_>>()
 }
 
-pub fn xor_cipher(hex1: String) -> String {
-    let cipher_byte_array = hex::decode(hex1).expect("INVALID HEX");
-    let best_byte_array = find_best_score(&cipher_byte_array);
-    String::from_utf8(best_byte_array.to_vec()).expect("INVALID UTF-8")
+
+pub fn get_score(xor: &Vec<u8>) -> u32 {
+	let frequency: HashMap<u8, u32> = [
+		(b'a', 8),
+		(b'e', 12),
+		(b'h', 6),
+		(b'i', 7),
+		(b'n', 7),
+		(b'o', 8),
+		(b't', 8),
+		(b's', 6),
+		(b'r', 6),
+		(b'd', 4),
+		(b'l', 4),
+		(b'c', 3),
+		(b'u', 3),
+		(b' ', 12),
+	]
+	.iter().cloned().collect();
+
+	// Check if there are only ASCII values
+	if xor.len() == 0 || !xor.iter().all(|c| c < &127) {
+		return 0;
+	}
+
+	xor.iter().map(|b| frequency.get(&b.to_ascii_lowercase()).unwrap_or(&0)).sum()
 }
 
-pub fn find_best_score(byte_array: &Vec<u8>) -> Vec<u8>{
+fn find_key(encrypted: &[u8]) -> u8 {
+    let mut round_key = 0;
+    let mut round_score = 0;
+    let mut best = (0, 0);
+    let mut xored = Vec::new();
 
+    while round_key < 128 {
+
+        round_score = 0;
+        xored = xor_repeats(encrypted, &round_key);
+        round_score += get_score(&xored);
+
+        if best.1 < round_score {
+            best.0 = round_key;
+            best.1 = round_score;
+        }
+        round_key += 1;
+    }
+
+    best.0
 }
 
-fn guess_the_guess() {
 
+fn break_xor_cipher(input: String) -> String {
+    let byte_array = hex::decode(input).unwrap();
+    let key = find_key(&byte_array);
+    String::from_utf8(xor_repeats(&byte_array, &key)).unwrap()
 }
 
-pub fn score_guess() {
-
-}
 
 #[cfg(test)]
 mod tests {
@@ -88,8 +97,7 @@ mod tests {
     #[test]
     fn test_hex_to_base64() {
     
-    String::from_        assert_eq!(
-
+    assert_eq!(
             hex_to_base64(String::from("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")),
             Ok(String::from("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"))
         );
@@ -106,17 +114,13 @@ mod tests {
         );
     }
 
-    #[test]
-    fn test_xor_cipher() {
-        let s = match xor_cipher(
-            String::from("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"),
-            &0b0000_0101,
-        ) {
-            Ok(v) => v,
-            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
-        };
 
-        println!("result: {}", s);
+    #[test]
+    fn test_xor() {
+        assert_eq!(
+            break_xor_cipher(String::from("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")),
+            String::from("Cooking MC's like a pound of bacon")
+        )
     }
 
 }
